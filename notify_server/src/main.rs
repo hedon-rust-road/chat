caec2b5c820a7be737f1f60c362f6b2deaee011a
hedon954 <171725713@qq.com>
2024-06-08 14:33:27 +0800

    feat: initial notify server skeleton.

--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use notify_server::get_router;
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let app = get_router();
+    let addr = "0.0.0.0:6687";
+    let listener = TcpListener::bind(addr).await?;
+    info!("Listening on {}", addr);
+
+    axum::serve(listener, app).await?;
+    Ok(())
 }
